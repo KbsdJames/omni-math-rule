b9b64896b98319b66355a4a7d1590c529ab7d73d
高博飞 <gaobofei@msh.team>
2024-12-22 13:38:13 +0000

    remov absolute_path

--- evaluation/eval.sh
+++ evaluation/eval.sh
@@ -1,9 +1,4 @@
-# EXP_NAME="QwQ-32B-Preview"
-# DATASET_TYPE="gsm8k"
-# INPUT_PATH="/mnt/moonfs/gaobofei-m2/inference/results/GSM8K_test_QwQ-32B-Preview.jsonl"
-# bash sh/eval.sh $DATASET_TYPE $INPUT_PATH $EXP_NAME
-
 EXP_NAME="QwQ-32B-Preview"
 DATASET_TYPE="omni-math"
-INPUT_PATH="/mnt/moonfs/gaobofei-m2/inference/results/OmniMATH_test_QwQ-32B-Preview.jsonl"
+INPUT_PATH="path/to/your/results/OmniMATH_rule_test_QwQ-32B-Preview.jsonl"
 bash sh/eval.sh $DATASET_TYPE $INPUT_PATH $EXP_NAME
